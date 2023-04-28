d8e6b92b07a370f134774c36fd122f8d34be631b
Kleiton Lima <98989110+KleitonLima@users.noreply.github.com>
2023-04-28 01:19:34 -0300

    feat: add Head titlte in first-post

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,8 +1,12 @@
 import Link from "next/link";
+import Head from "next/head";
 
 export default function FirstPost() {
   return (
     <>
+      <Head>
+        <title>Primeiro Post</title>
+      </Head>
       <h1>Primeiro Post</h1>
       <h2>
         <Link href="/">voltar para home</Link>
