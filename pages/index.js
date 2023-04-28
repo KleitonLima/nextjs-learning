72f33ea889c0dad1d0a26c2f6e2e51be97f17ff2
Kleiton Lima <98989110+KleitonLima@users.noreply.github.com>
2023-04-28 00:12:59 -0300

    feat(index/pages): edite title

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,13 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Create Next</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Learning <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
